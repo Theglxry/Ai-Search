f113c51bbe3d1b8d7329520d37fc433dbed27a87
Theglxry <102861719+Theglxry@users.noreply.github.com>
2023-12-04 12:48:44 +0100

    ""

--- src/components/homeComp/NavBar.jsx
+++ src/components/homeComp/NavBar.jsx
@@ -15,28 +15,31 @@ const navBar = () => {
 
   return (
     <>
-      <nav className="w-full flex py-6 justify-between items-center navbar ">
-        {/* <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" /> */}
+      <nav className="w-full flex py-8 justify-between items-center"
+      >
         <div className="text-white font-panchang text-4xl font-bold">
           A.ISearch
         </div>
 
+
+
         <div className="sm:block hidden">
-          <ul className="list-none flex justify-center2 items-center flex-1">
+          <ul className="list-none flex justify-center items-center sm:text-xl gap-8 flex-1">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-poppins sm:flex sm:items-center font-nomal cursor-pointer text-[16px] 
+                className={`sm:flex sm:items-center cursor-pointer 
             ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}
 
              text-white`}
               >
-                <img className="w-6 h-6" src={nav.img} />
+                <img className="" src={nav.img} />
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
             ))}
           </ul>
         </div>
+
 
         <div className="sm:flex hidden font-redHatDisplay text-sm md:text-base font-bold">
           <a
@@ -115,53 +118,7 @@ const navBar = () => {
 
 
 
-       {/* old nav */}
-      {/* <nav className="flex justify-between w-full pt-8">
-        <div className="text-white font-panchang text-4xl font-bold">
-          A.ISearch
-        </div>
-        <div>
-          <ul className="sm:flex flex-row gap-8 text-white hidden ">
-            <li
-              className="flex rounded-lg border border-darkGray items-center   px-3 py-3"
-              style={{ gap: "4px" }}
-            >
-              <img src={gridPurple} />
-              Discover
-            </li>
-            <li
-              className="flex rounded-lg border border-darkGray items-center   px-3 py-3"
-              style={{ gap: "4px" }}
-            >
-              <img src={bookmark} />
-              Favorite
-            </li>
-            <li
-              className="flex rounded-lg border border-darkGray items-center   px-3 py-3"
-              style={{ gap: "4px" }}
-            >
-              <img src={collectionIcon} />
-              Blog
-            </li>
-          </ul>
-        </div>
 
-        <div className="sm:flex hidden font-redHatDisplay text-sm md:text-base font-bold">
-          <a
-            href="#"
-            className="flex rounded-lg border border-darkGray items-center bg-tertiary px-3 py-3"
-            style={{ gap: "4px" }}
-          >
-            <img
-              src={plusCircle}
-              className="w-6 h-6"
-              alt="grid discover"
-              style={{ marginRight: "8px" }}
-            />{" "}
-            Submit Tool
-          </a>
-        </div>
-      </nav> */}
     </>
   );
 };
